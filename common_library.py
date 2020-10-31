42f8beb6c88af8ce99f549ad935b97cfbb6906b4
RyuMurase <murase.ryu.86c@gmail.com>
2020-10-31 16:57:41 +0900

    created common library

---
+++ common_library.py
@@ -0,0 +1,30 @@
+#CONSTANTS(atomic unit)
+a_0 = 0.529 #Bohr radius(A)
+E_0 = 27.2 #Hartree energy(eV)
+e2 = 14.4 #(eV A)
+E_PROTON = 25
+Z_PROTON = 1 #atomic number of proton
+E_CARBON = 300 #(keV at 1 au velocity)
+Z_CARBON = 6 #atomic number of carbon
+A = 0.240 # constant for calculating screening length of Brandt-Kitagawa model
+
+#constanta for Thomas-Fermi-Morliere screening function
+TMF = {
+    "alpha": [0.10, 0.55, 0.35],
+    "beta": [6.0, 1.20, 0.30]
+}
+
+#convert index to index string
+def str_ind(i, j):
+    res = str(i) + str(j)
+    if res > res[::-1]:res = res[::-1]
+    return res
+
+#Drude-type Optical energy loss function
+def drude_function(x):
+    Z_ave = 4 #for Glycine
+    #parameters(from Z. Tan el al, Rad.Env.Biophys. 45 2(2006) 135-143)
+    a = 0.2882925381282788 # defined to satisfy f-sum rule
+    b = (19.927 + 0.9807 * Z_ave) / E_0
+    c = (13.741 + 0.3215 * Z_ave) / E_0
+    return a*x/((x**2-b**2)**2 + (c*x)**2)
