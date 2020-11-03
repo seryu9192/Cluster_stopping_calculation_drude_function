0deb22283c868a8166204eb4c9673c25348630bb
RyuMurase <murase.ryu.86c@gmail.com>
2020-11-03 19:25:27 +0900

    changed to read CONSTANTS from common file changed unit to atomic unit utilized Drude-type ELF changed variable kappa to k

--- C3/1_C3_linear_calc_stopping_BK.py
+++ C3/1_C3_linear_calc_stopping_BK.py
@@ -7,20 +7,14 @@ from scipy import integrate
 import os
 import sys
 import json
+sys.path.append('../')
+from common_library import *
 
 #directory path
 working_dir =  r'./'
 input_dir = os.path.join(working_dir, 'results')
 q_ave_path = os.path.join(input_dir, '0_C3_linear_average_charge.txt')
-param_path = r'./param_C3_linear.json'
-
-#CONSTANTS
-a_0 = 0.529 #Bohr radius(A)
-E_Ryd = 13.6 #Rydberg energy(eV)
-e2 = 14.4 #(eV A)
-E_CARBON = 300 #(keV at 1 au velocity)
-Z_CARBON = 6 #atomic number of carbon
-A = 0.240 # BKモデルの遮蔽定数の計算のための定数
+param_path = r'./param_C3.json'
 
 #calculation condition
 dtheta = 1 #theta step
@@ -29,93 +23,91 @@ thetas = [dtheta * i for i in range(theta_max//dtheta + 1)] #[0deg ~ 90deg, step
 
 #experimental condition
 # Energy(keV) and velocity(au) of projectile
-E_0 = 900
-
+E = 0
 #target name
 target = ''
-
-#plasmon energy(eV)
-E_p = 20
+#plasmon energy(atomic unit)
+E_p = 0
 
 #parameters for integration
 r_close = 0
 r_dist = 0
 k_max = 0
-d = [0, 0]
-b = [0, 0]
-N = [0, 0]
-q = [0, 0]
-lamb = [0]
+k_min = 0
+d = {"00":0}
+b = {"00":0}
 r = {"00":0}
-t = 0
-v = sqrt(E_0/E_CARBON)
+N = [0]
+q = [0]
+lamb = [0]
 
-def str_ind(i, j):
-    res = str(i) + str(j)
-    if res > res[::-1]:res = res[::-1]
-    return res
+#beam velocity v (atomic unit)
+v = 0
 
 #被積分関数
-def func(k):
+def integrand(k, w):
+    w_k = w + k**2/2
+    kappa = (k**2-(w_k/v)**2)**(1/2)
+
     #Brandt-Kitagawaモデルの遮蔽関数（のFourier変換）
     zeta = [0] * len(q)
     
     #calc each zeta
     for i in range(len(q)):
-        zeta[i] = (q[i] + ((1/r_dist)**2 + k**2)*(lamb[i]**2))/(1 + ((1/r_dist)**2 + k**2)*(lamb[i]**2))
+        zeta[i] = (q[i] + (k*lamb[i])**2)/(1 + (k*lamb[i])**2)
 
-    #calc diagonal and cross_terms term
+    #calc diagonal and cross term
     diagonal_terms = 0
     cross_terms = 0
+    #diag
     for i in range(len(q)):
-       diagonal_terms += zeta[i]**2
+       diagonal_terms += zeta[i]**2 
+    #cross
     for i in range(len(q)):
         for j in range(len(q)):
             if i >= j:
                 continue
-            cross_terms += 2* zeta[i]*zeta[j]*j0(k * b[str_ind(i, j)])*cos(1/r_dist * d[str_ind(i, j)])
-    return k * (diagonal_terms + cross_terms)/(k**2 + 1/r_dist**2)
+            cross_terms += 2*zeta[i]*zeta[j]*j0(kappa * b[str_ind(i, j)])*cos(w_k/v * d[str_ind(i, j)])
+    elf = drude_function(w)
+    return w * (diagonal_terms + cross_terms)*elf/k
 
 #calc stopping
 def calc_stopping_BK(t):
-    global d
-    global b
-
     # projectile C3+ の幾何学的な配置
+    global d, b
     d = {k: v * cos(t) for k, v in r.items()} #axial projection
     b = {k: v * sin(t) for k, v in r.items()} #radial projection
 
     #integration result
-    res, err = integrate.quad(func, 0, k_max)
+    res = integrate.dblquad(integrand, 0, v**2/2, lambda x: v*(1-(1-2*x/v**2)**(1/2)), lambda x: v*(1+(1-2*x/v**2)**(1/2)))[0]
 
     #calc stopping
     res *= Z_CARBON**2
-    res *= e2
-    res /= r_dist**2
+    res *= 2/pi/v**2
+    res *= e2/a_0**2 # to eV/A
     return res
 
 def set_parameters(path):
-    global E_0, E_p, target
-    global r_close
-    global r_dist 
-    global k_max
+    global E, E_p, target
+    global r_close, r_dist 
+    global k_min, k_max
     global N
     global q
     global lamb
     global r
-    global t
     global v
 
     #import parameters
     with open(param_path, 'r') as f:
         params = json.loads(f.read())
     #set projectile parameters
-    E_0 = params["E0"]
-    v = sqrt(E_0/E_CARBON)
-    r = params["r"]
+    E = params["E0"]
+    v = sqrt(E/E_CARBON)
+    r = {k:v/a_0 for k, v in params["r"]["linear"].items()} # to atomic unit
+ 
     #set target parameters
     target = params["target"]
-    E_p = params["Ep"][target]
+    E_p = params["Ep"][target]/E_0 #to atomic unit
     
     #import average charge as np.array
     with open(q_ave_path, 'r') as f:
@@ -126,32 +118,35 @@ def set_parameters(path):
     # q : イオンの価数を電離度(0 - 1)に変換 -> BKモデルのqに対応
     q /= Z_CARBON
 
-    #Brandt-Kitagawaモデルの遮蔽定数 lambda
+    #Brandt-Kitagawaモデルの遮蔽定数 lambda (atomic unit)
     lamb = [0] * len(q)
     for i in range(len(q)):
-        lamb[i] = 2 * A * (N[i]/Z_CARBON)**(2/3)/(Z_CARBON**(1/3)*(1-N[i]/Z_CARBON/7)) * a_0
+        lamb[i] = 2 * A * (N[i]/Z_CARBON)**(2/3)/(Z_CARBON**(1/3)*(1-N[i]/Z_CARBON/7))
     
-    #calc r_close, r_dist
-    r_close = a_0/2/v
-    r_dist = 2*v*E_Ryd/E_p*a_0
-    k_max = sqrt((1/r_close)**2 - (1/r_dist)**2)
+    #calc r_close, r_dist(atomic unit)
+    r_close = 1/2/v
+    r_dist = v/E_p
 
+    #calc k range(atomic unit)
+    k_min = 1/r_dist
+    k_max = 1/r_close
     return
 
 def main():
     set_parameters(param_path)
     #check parameters
+    print('E = {} keV/atom'.format(E))
+    print('v = {} au'.format(v))
     print('target: {}'.format(target))
-    print('E0 = {} keV/atom'.format(E_0))
-    print('v0 = {} au'.format(v))
+    print('E_p = {} eV'.format(E_p))
     print('N : ', N)
     print('q : ', q)
     print('r : ', r)
     print('lambda : ', lamb)
-    print('r_dist = {} A'.format(r_dist))
-    print('r_close = {} A'.format(r_close))
-    print('k_max = {} A'.format(k_max))
-    print('E_p = {} eV'.format(E_p))
+    print('r_dist = {} au'.format(r_dist))
+    print('r_close = {} au'.format(r_close))
+    print('k_min = {} au'.format(k_min))
+    print('k_max = {} au'.format(k_max))
     
     results = []
     #配向角thetaについてループ
@@ -165,12 +160,12 @@ def main():
         results.append(stopping)
             
     #ファイルに書き込み
-    output_filename = 'E={}keV_atom_C3_linear_{}.txt'.format(E_0, target)
+    output_filename = 'E={}keV_atom_C3_linear_{}.txt'.format(E, target)
     with open(os.path.join(input_dir, output_filename), 'w') as f:
         #headerの書き込み
         #thetaについてループ
         header = ''
-        for i, theta in enumerate(thetas):
+        for theta in thetas:
             header += str(theta) + '\t'
         header += '\n'
         f.write(header)
