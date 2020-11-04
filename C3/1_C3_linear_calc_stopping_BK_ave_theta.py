dae3d65ef708e63aa688d4490c63806c176a2abf
RyuMurase <murase.ryu.86c@gmail.com>
2020-11-04 19:49:34 +0900

    changed to calc stopping averaged over theta

--- C3/1_C3_linear_calc_stopping_BK_ave_theta.py
+++ C3/1_C3_linear_calc_stopping_BK_ave_theta.py
@@ -1,4 +1,5 @@
-#0_C3_calc_stopping_BK.py: Brandt-Kitagawaモデルを3原子クラスターの阻止能公式に適用し、イオンに対する阻止能を計算する(積分はscipy.integrateを利用)
+#1_C3_linear_calc_stopping_BK.py: Brandt-Kitagawaモデルを3原子クラスターの阻止能公式に適用し、イオンに対する阻止能を計算する(積分はscipy.integrateを利用)
+# stopping averaged over theta
 
 import numpy as np
 from numpy import sqrt, sin, cos, pi, log, radians
@@ -16,11 +17,6 @@ input_dir = os.path.join(working_dir, 'results')
 q_ave_path = os.path.join(input_dir, 'C3_average_charge.json')
 param_path = r'./param_C3.json'
 
-#calculation condition
-dtheta = 1 #theta step
-theta_max = 90 #theta max
-thetas = [dtheta * i for i in range(theta_max//dtheta + 1)] #[0deg ~ 90deg, step=dtheta]
-
 #experimental condition
 # Energy(keV) and velocity(au) of projectile
 E = 0
@@ -34,8 +30,6 @@ r_close = 0
 r_dist = 0
 k_max = 0
 k_min = 0
-d = {"00":0}
-b = {"00":0}
 r = {"00":0}
 N = [0]
 q = [0]
@@ -46,9 +40,6 @@ v = 0
 
 #被積分関数
 def integrand(k, w):
-    w_k = w + k**2/2
-    kappa = (k**2-(w_k/v)**2)**(1/2)
-
     #Brandt-Kitagawaモデルの遮蔽関数（のFourier変換）
     zeta = [0] * len(q)
     
@@ -67,17 +58,12 @@ def integrand(k, w):
         for j in range(len(q)):
             if i >= j:
                 continue
-            cross_terms += 2*zeta[i]*zeta[j]*j0(kappa * b[str_ind(i, j)])*cos(w_k/v * d[str_ind(i, j)])
+            cross_terms += 2*zeta[i]*zeta[j]*sin(k * r[str_ind(i, j)])/(k * r[str_ind(i, j)]) #averaged over theta
     elf = drude_function(w)
     return w * (diagonal_terms + cross_terms)*elf/k
 
 #calc stopping
-def calc_stopping_BK(t):
-    # projectile C3+ の幾何学的な配置
-    global d, b
-    d = {k: v * cos(t) for k, v in r.items()} #axial projection
-    b = {k: v * sin(t) for k, v in r.items()} #radial projection
-
+def calc_stopping_BK():
     #integration result
     res = integrate.dblquad(integrand, 0, v**2/2, lambda x: v*(1-(1-2*x/v**2)**(1/2)), lambda x: v*(1+(1-2*x/v**2)**(1/2)))[0]
 
@@ -149,33 +135,13 @@ def main():
     print('k_min = {} au'.format(k_min))
     print('k_max = {} au'.format(k_max))
     
-    results = []
-    #配向角thetaについてループ
-    for t_deg in thetas:
-        t_rad = radians(t_deg)
-        stopping = calc_stopping_BK(t_rad)
-        if t_deg % 5 == 0:
-            print('t = {} deg'.format(t_deg))
-            print('d : ', d) 
-            print('b : ', b) 
-        results.append(stopping)
-            
-    #ファイルに書き込み
-    output_filename = 'E={}keV_atom_C3_linear_{}.txt'.format(E, target)
-    with open(os.path.join(input_dir, output_filename), 'w') as f:
-        #headerの書き込み
-        #thetaについてループ
-        header = ''
-        for theta in thetas:
-            header += str(theta) + '\t'
-        header += '\n'
-        f.write(header)
 
-        line = ''
-        for stopping in results:
-            line += str(stopping) + '\t'
-        line += '\n'
-        f.write(line)
+    stopping = calc_stopping_BK()
+    print(f"S = {stopping} eV/A")
+    #ファイルに書き込み
+    output_filename = 'E={}keV_atom_C3_linear_{}_ave_theta.txt'.format(E, target)
+    with open(os.path.join(input_dir, output_filename), 'w') as f:
+        f.write(f"{stopping}")
     print('successfully written to {}'.format(os.path.join(input_dir, output_filename)))
     print('finished!')
 
