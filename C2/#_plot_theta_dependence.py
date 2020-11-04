ebd83915be7cf970f70b145c23b678b0d8440494
RyuMurase <murase.ryu.86c@gmail.com>
2020-11-04 19:48:24 +0900

    changed expression of the y-scale changed format of the figure caption

--- C2/#_plot_theta_dependence.py
+++ C2/#_plot_theta_dependence.py
@@ -26,12 +26,13 @@ plt.rcParams["font.size"] = 18
 plt.rcParams["axes.linewidth"] = 1.5                
 
 target = 'Gly'
+E = 900
 
 #filepath
 working_dir =  r'results'
 fig_dir = r'../fig'
 input_dir = working_dir
-filename = 'E=900keV_atom_C2_linear_{}.txt'.format(target)
+filename = 'E={}keV_atom_C2_linear_{}_wk=w.txt'.format(E, target)
 inputfile_path = os.path.join(input_dir, filename)
 
 def main():
@@ -51,16 +52,20 @@ def main():
         ax.set_xlim(0, 90)
         ax.xaxis.set_major_locator(MultipleLocator(10))
         ax.xaxis.set_minor_locator(MultipleLocator(5))
+
         #y-axis
+        y_min, y_max, y_major, y_minor = 0, 300, 50, 10
         ax.set_ylabel('', fontsize=22)
         ax.set_ylabel('SP (eV/$\AA$)', fontsize=22)
-        ax.set_ylim(0, 300)
-        ax.yaxis.set_major_locator(MultipleLocator(50))
-        ax.yaxis.set_minor_locator(MultipleLocator(10))
+        ax.set_ylim(y_min, y_max)
+        ax.yaxis.set_major_locator(MultipleLocator(y_major))
+        ax.yaxis.set_minor_locator(MultipleLocator(y_minor))
+        
         #plot
         ax.plot(xs, ys, linewidth=2, color='black')
         
-    ax.text(0.02, 0.9, '0.9 MeV/atom C$_2^+$ in {}'.format(target), fontsize=24, transform=ax.transAxes)
+    # ax.text(0.02, 0.9, '{:.1f} MeV/atom C$_2^+$ in {}'.format(E/1000,target), fontsize=24, transform=ax.transAxes)
+    ax.text(0.01, 0.9, filename, fontsize=24, transform=ax.transAxes)
 
     save_fig_on = False
     if save_fig_on:
--- C4/#_plot_theta_dependence.py
+++ C4/#_plot_theta_dependence.py
@@ -31,7 +31,7 @@ target = 'Gly'
 working_dir =  r'results'
 fig_dir = r'../fig'
 input_dir = working_dir
-filename = 'E=900keV_atom_C4_linear_{}.txt'.format(target)
+filename = 'E=900keV_atom_C4_linear_{}_wk=w.txt'.format(target)
 inputfile_path = os.path.join(input_dir, filename)
 
 def main():
@@ -51,16 +51,20 @@ def main():
         ax.set_xlim(0, 90)
         ax.xaxis.set_major_locator(MultipleLocator(10))
         ax.xaxis.set_minor_locator(MultipleLocator(5))
+
         #y-axis
+        y_min, y_max, y_major, y_minor = 0, 300, 50, 10
         ax.set_ylabel('', fontsize=22)
         ax.set_ylabel('SP (eV/$\AA$)', fontsize=22)
-        ax.set_ylim(0, 300)
-        ax.yaxis.set_major_locator(MultipleLocator(50))
-        ax.yaxis.set_minor_locator(MultipleLocator(10))
+        ax.set_ylim(y_min, y_max)
+        ax.yaxis.set_major_locator(MultipleLocator(y_major))
+        ax.yaxis.set_minor_locator(MultipleLocator(y_minor))
+        
         #plot
         ax.plot(xs, ys, linewidth=2, color='black')
-        
-    ax.text(0.02, 0.9, '0.9 MeV/atom C$_4^+$ in {}'.format(target), fontsize=24, transform=ax.transAxes)
+
+    ax.text(0.01, 0.9, filename, fontsize=24, transform=ax.transAxes)        
+    # ax.text(0.02, 0.9, '{:.1f} MeV/atom C$_4^+$ in {}'.format(E/1000,target), fontsize=24, transform=ax.transAxes)        
 
     save_fig_on = False
     if save_fig_on:
