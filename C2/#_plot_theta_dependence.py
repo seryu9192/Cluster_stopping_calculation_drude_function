3c676a22290de338b19d37b37915042c6c7584b9
RyuMurase <murase.ryu.86c@gmail.com>
2021-04-18 21:02:40 +0900

    fix: to plot interference component

--- C2/#_plot_theta_dependence.py
+++ C2/#_plot_theta_dependence.py
@@ -25,14 +25,16 @@ plt.rcParams["ytick.minor.size"] = 3
 plt.rcParams["font.size"] = 18
 plt.rcParams["axes.linewidth"] = 1.5                
 
-E = 500
+colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
+
+E = 1800
 target = 'Gly'
 
 #filepath
 working_dir =  r'results'
-fig_dir = r'./figs'
+fig_dir = r'.\figs'
 input_dir = working_dir
-filename = f'E={E}keV_atom_C2_linear_Gly_r=2.40.txt'
+filename = f'E={E}keV_atom_C2_linear_Gly_interfere.txt'
 inputfile_path = os.path.join(input_dir, filename)
 
 def main():
@@ -44,18 +46,19 @@ def main():
     
     fig = plt.figure(figsize=(9, 6), dpi=300)
     ax = fig.add_subplot(1, 1, 1)
+    labels = ['$S_{\mathrm{A}}$', '$S_{\mathrm{B}}$', '$S_{\mathrm{A}}+S_{\mathrm{B}}$']
     for i in range(len(dat)):
         ys = [float(v) for v in dat[i]]
         
         #x-axis
-        ax.set_xlabel('Orientation angle (degree)', fontsize=22)
+        ax.set_xlabel('Orientation angle (degrees)', fontsize=22)
         x_min, x_max, x_major, x_minor = 0, 90, 10, 5
         ax.set_xlim(x_min, x_max)
         ax.xaxis.set_major_locator(MultipleLocator(x_major))
         ax.xaxis.set_minor_locator(MultipleLocator(x_minor))
 
         #y-axis
-        y_min, y_max, y_major, y_minor = 0, 300, 50, 10
+        y_min, y_max, y_major, y_minor = -20, 200, 50, 10
         ax.set_ylabel('', fontsize=22)
         ax.set_ylabel('SP (eV/$\AA$)', fontsize=22)
         ax.set_ylim(y_min, y_max)
@@ -63,12 +66,13 @@ def main():
         ax.yaxis.set_minor_locator(MultipleLocator(y_minor))
         
         #plot
-        ax.plot(xs, ys, linewidth=2, color='black')
+        ax.plot(xs, ys, linewidth=2, color=colors[i], label=labels[i])
         
-    ax.text(0.02, 0.9, '{:.1f} MeV/atom C$_2^+$ in {}'.format(E/1000,target), fontsize=24, transform=ax.transAxes)
+    ax.text(0.02, 0.9, f'{E/1000*2:.1f} MeV C$_2^+$ in {target}', fontsize=24, transform=ax.transAxes)
+    ax.legend()
     # ax.text(0.01, 0.9, filename, fontsize=16, transform=ax.transAxes)
 
-    save_fig_on = True
+    save_fig_on = False
     if save_fig_on:
         os.makedirs(fig_dir, exist_ok=True)
         fig_path = os.path.join(fig_dir, filename[:-4]+'.png')
