053270ec957abc17f744d4d770687d235df55ef7
RyuMurase <murase.ryu.86c@gmail.com>
2021-08-20 22:23:33 +0900

    first commit

---
+++ H/0_H_average_charge.py
@@ -0,0 +1,68 @@
+# H_average_charge.py : calculate H average charge using Kaneko model
+# Ref: T. Kaneko et al, NIMB315(2013)76
+
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import pi, sqrt, exp
+from scipy import integrate
+import os
+import sys
+import json
+sys.path.append('../')
+from common_library import *
+
+#filepath
+working_dir = './'
+param_filename = 'param_H.json'
+param_path = os.path.join(working_dir, param_filename)
+output_dir = 'results'
+
+#parameters
+E = 0
+v = 0
+
+#interatomic dist in A
+r = 0 #in au
+q_init = 1
+qs = [q_init]
+
+#NUMBER OF ITERATION
+NUM_ITER = 100
+
+def read_parameters(path):
+    global E, v
+    with open(path, 'r') as f:
+        params = json.loads(f.read())
+    E = params["E0"]
+    v = sqrt(E/E_PROTON)
+    return
+
+def main():
+    read_parameters(param_path)
+
+    #average speed of bound electrons
+    v_bind = 1.045*Z_PROTON**(2/3)
+
+    # calc average charge
+    q = Z_PROTON * 2/sqrt(pi) * integrate.quad(lambda x: exp(-x**2), 0, sqrt(3/8)*v/v_bind)[0]
+    print("E = {} keV".format(E))
+    print("v = {} au".format(v))    
+    print("q_ave =", q)
+
+    #save as a json file
+    output_filename = 'H_average_charge.json'
+    output_path = os.path.join(output_dir, output_filename)
+    #read json file if exists
+    try:
+        with open(output_path, 'r') as f:
+            dat = json.loads(f.read())
+    except Exception:
+        dat = {}
+        os.makedirs(output_dir, exist_ok=True)
+    #update json
+    dat[f"{E}"] = q
+    with open(output_path, 'w') as f:
+        f.write(json.dumps(dat, indent=4))
+
+if __name__ == '__main__':
+    main()
---
+++ H/0_H_average_charge_veloDep.py
@@ -0,0 +1,59 @@
+# H1_average_charge.py : calculate H1 average charge using Kaneko model
+# Ref: T. Kaneko et al,NIMB315(2013)76
+
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import pi, sqrt, exp
+from scipy import integrate
+
+plt.rcParams["font.family"] = "Arial"
+plt.rcParams["xtick.direction"] = "out"             
+plt.rcParams["ytick.direction"] = "in"              
+plt.rcParams["xtick.minor.visible"] = True
+plt.rcParams["ytick.minor.visible"] = True          
+plt.rcParams["xtick.major.width"] = 1.5             
+plt.rcParams["ytick.major.width"] = 1.5             
+plt.rcParams["xtick.minor.width"] = 1.0             
+plt.rcParams["ytick.minor.width"] = 1.0             
+plt.rcParams["xtick.major.size"] = 6                
+plt.rcParams["ytick.major.size"] = 6                
+plt.rcParams["xtick.minor.size"] = 3
+plt.rcParams["ytick.minor.size"] = 3
+plt.rcParams["font.size"] = 18                      
+plt.rcParams["axes.linewidth"] = 1.5                
+
+#proton
+Z = 1
+E_C = 25
+E_0 = 0
+v_0 = sqrt(E_0/E_C)
+
+def gauss(t):
+    return exp(-t**2)
+
+def main():
+    #proton
+    Z = 1
+    v_bind = 1.045*Z**(2/3)
+
+    # calc average charge
+    vs = np.linspace(0, 10, 100)
+    ys = [Z*2/sqrt(pi)*integrate.quad(gauss, 0, sqrt(3/8)*v/v_bind)[0] for v in vs]
+    #plot
+    fig = plt.figure(dpi=300)
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(vs, ys)    
+    #x axis
+    ax.set_xlim(0, 10)
+    ax.set_xlabel('$v/v_0$')
+    #y axis
+    ax.set_ylim(0, 1.1)
+    ax.set_ylabel('$q_{ave}$')
+    #text
+    ax.text(0.05, 0.9, 'average charge of H', transform=ax.transAxes)
+    q_ave = Z*2/sqrt(pi)*integrate.quad(gauss, 0, sqrt(3/8)*v_0/v_bind)[0] 
+    
+    print(q_ave)
+    
+if __name__ == '__main__':
+    main()
